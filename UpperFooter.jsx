fa1bffd89bc91df99bca96dde3df55f5b58e6f56
Ranajit Das <101388961+ranajitdasgithub@users.noreply.github.com>
2022-07-22 01:56:01 +0530

    Footer page added

---
+++ UpperFooter.jsx
@@ -0,0 +1,92 @@
+import { Box, Center, Tab, TabList, TabPanel, TabPanels, Tabs, Text } from "@chakra-ui/react"
+
+
+function UpperFooter(){
+    return(<>
+    <Box w="90%" border="1px solid" h='250px' m='auto' my='3rem' borderRadius='10px' borderColor="gray.300">
+        <Box w='98%' m='auto'>
+            <Tabs  borderBottom='1px' borderColor='gray.300'>
+                <Tab>Be inspired and discover more with Expedia</Tab>
+            </Tabs>
+            <Box ml='15px' textAlign='start' h='190px' overflowY='scroll' >
+                <Text fontSize='lg' fontWeight='500'>Hotel deals on top domestic destinations</Text>
+                <Box ml='12px' fontSize='14px' color='blue'>
+                    <Text>Hotels</Text>
+                    <Text>New York hotels</Text>
+                    <Text>Miami hotels</Text>
+                    <Text>Chicago hotels</Text>
+                    <Text>Myrtle Beach hotels</Text>
+                    <Text>Key West hotels</Text>
+                </Box>
+                <Text fontSize='lg' fontWeight='500'>Books hotels Internationally</Text>
+                <Box ml='12px' fontSize='14px' color='blue'>
+                    <Text>Cabu San Lucas hotels</Text>
+                    <Text>Paris hotels</Text>
+                    <Text>Cancun hotels</Text>
+                    <Text>Tokyo hotels</Text>
+                    <Text>Los Cabos hotels</Text>
+                    <Text>London hotels</Text>
+                </Box>
+                <Text fontSize='lg' fontWeight='500'>Hotel deals on top domestic destinations</Text>
+                <Box ml='12px' fontSize='14px' color='blue'>
+                    <Text>Hotels</Text>
+                    <Text>New York hotels</Text>
+                    <Text>Miami hotels</Text>
+                    <Text>Chicago hotels</Text>
+                    <Text>Myrtle Beach hotels</Text>
+                    <Text>Key West hotels</Text>
+                </Box>
+                <Text fontSize='lg' fontWeight='500'>Books hotels Internationally</Text>
+                <Box ml='12px' fontSize='14px' color='blue'>
+                    <Text>Cabu San Lucas hotels</Text>
+                    <Text>Paris hotels</Text>
+                    <Text>Cancun hotels</Text>
+                    <Text>Tokyo hotels</Text>
+                    <Text>Los Cabos hotels</Text>
+                    <Text>London hotels</Text>
+                </Box>
+                <Text fontSize='lg' fontWeight='500'>Hotel deals on top domestic destinations</Text>
+                <Box ml='12px' fontSize='14px' color='blue'>
+                    <Text>Hotels</Text>
+                    <Text>New York hotels</Text>
+                    <Text>Miami hotels</Text>
+                    <Text>Chicago hotels</Text>
+                    <Text>Myrtle Beach hotels</Text>
+                    <Text>Key West hotels</Text>
+                </Box>
+                <Text fontSize='lg' fontWeight='500'>Books hotels Internationally</Text>
+                <Box ml='12px' fontSize='14px' color='blue'>
+                    <Text>Cabu San Lucas hotels</Text>
+                    <Text>Paris hotels</Text>
+                    <Text>Cancun hotels</Text>
+                    <Text>Tokyo hotels</Text>
+                    <Text>Los Cabos hotels</Text>
+                    <Text>London hotels</Text>
+                </Box>
+                <Text fontSize='lg' fontWeight='500'>Hotel deals on top domestic destinations</Text>
+                <Box ml='12px' fontSize='14px' color='blue'>
+                    <Text>Hotels</Text>
+                    <Text>New York hotels</Text>
+                    <Text>Miami hotels</Text>
+                    <Text>Chicago hotels</Text>
+                    <Text>Myrtle Beach hotels</Text>
+                    <Text>Key West hotels</Text>
+                </Box>
+                <Text fontSize='lg' fontWeight='500'>Books hotels Internationally</Text>
+                <Box ml='12px' fontSize='14px' color='blue'>
+                    <Text>Cabu San Lucas hotels</Text>
+                    <Text>Paris hotels</Text>
+                    <Text>Cancun hotels</Text>
+                    <Text>Tokyo hotels</Text>
+                    <Text>Los Cabos hotels</Text>
+                    <Text>London hotels</Text>
+                </Box>
+                       
+            </Box>
+        </Box>
+    
+    </Box>
+    
+    </>)
+}
+export default UpperFooter
